9c281c1484c9bfcf0a4f712ded87d3981ebbd87c
Yosef Abramov <yosefamiami@gmail.com>
2025-03-31 13:22:22 -0400

    updated main

--- LINQ/Exercise3.cs
+++ LINQ/Exercise3.cs
@@ -11,6 +11,23 @@ namespace LinqExercise
 
         static void Main(string[] args)
         {
+            List<string> games = new List<string>
+            {
+                "Moshiach Man",
+                "Doing Good Deeds",
+                "Smiles On Faces",
+                "Yankel",
+                "More Light"
+
+            };
+
+            var orderedGames = games.OrderBy(game => game.Length);
+
+            Console.WriteLine("Games ordered by name length:");
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
             /*
              * 
              * Complete every task using Method OR Query syntax. 
